b4af032726581577dc8fb6f880e9adf912cfac3b
Bheemesh1706 <bheemeshbala.71@gmail.com>
2022-10-13 13:32:02 +0530

    removed

--- test.rb
+++
@@ -1,4 +0,0 @@
-
-NOTE:   For representing the small cars size use "Small" key word
-        For representing the medium cars size use "Medium" key word
-        For representing the large cars size use "Large" key word
